a47d22c924b98e31b2ddb5c9cffc4f1262a95b5c
Alex Lach <alexglach@gmail.com>
2016-09-16 14:21:07 -0400

    working on promises

--- script.js
+++ script.js
@@ -20,47 +20,93 @@
 var $ = {
 
   ajax: function(options) {
-    var xhr = new XMLHttpRequest();
 
-    // promise object
-    // default options
-    // var defaultOptions = {
-    //   contentType: "contentType",
-    //   headers: {},
-    // }
+    return new Promise( function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
 
-    xhr.onload = function(e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          options.success(this.responseText);
-        } else {
-          options.error();
+      // promise object
+      // default options
+      // var defaultOptions = {
+      //   contentType: "contentType",
+      //   headers: {},
+      // }
+
+      xhr.onload = function(e) {
+        if (xhr.readyState === 4) {
+          if (xhr.status >= 200 && xhr.status < 300) {
+            options.success(this.responseText);
+            resolve(this.responseText);
+          } else {
+            options.error();
+            reject(this.statusText);
+          }
+          options.complete();
         }
-        options.complete();
       }
-    }
 
-    xhr.onerror = function(e) {
-      options.error;
-    };
-    xhr.open(options.method, options.url, options.async);
-    // if (options.content) {
-    //   var contentType = options.content
-    // }
-    if (options.header) {
-      for (var key in options.header) {
-        xhr.setRequestHeader(key, options.headers[key]);
+      xhr.onerror = function(e) {
+        options.error;
+      };
+      xhr.open(options.method, options.url, options.async);
+      // if (options.content) {
+      //   var contentType = options.content
+      // }
+      if (options.header) {
+        for (var key in options.header) {
+          xhr.setRequestHeader(key, options.headers[key]);
+        }
       }
-    }
-    var url = "";
-    if (options.data) {
-      for (var key in options.data) {
-        url += key + "=" + options.data[key] + "&";
+      var url = "";
+      if (options.data) {
+        if (options.data instanceof String) {
+          url = options.data;
+        } else {
+          for (var key in options.data) {
+            url += key + "=" + options.data[key] + "&";
+          }
+        }
+        url = url.trim("&");
       }
-      url = url.trim("&");
-    }
-    xhr.send(url);
+      xhr.send(url);
+    });
   },
+
+  get: function( url, data, success) {
+    var getOptions = {
+      url: url,
+      method: "GET",
+      headers: {},
+      complete: function complete() {
+        console.log("I'm finished!");
+      },
+      error: function error() {
+        console.log("Couldn't get data");
+      },
+      success: success,
+      data: data,
+    };
+    $.ajax(getOptions);
+  },
+
+  post: function( url, data, success) {
+    var postOptions = {
+      url: url,
+      method: "POST",
+      data: data,
+      success: success,
+      complete: function complete() {
+        console.log("I'm finished!");
+      },
+      error: function error() {
+        console.log("Couldn't get data");
+      },
+      async: true,
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded"
+      }
+    }
+    $.ajax(postOptions);
+  }
 
 }
 
@@ -106,4 +152,5 @@ var optionsPost = {
 }
 
 
-$.ajax(optionsPost)
+$.post("http://reqres.in/api/users", "title=Foo&body=Bar&userId=1", optionsPost.success)
+
